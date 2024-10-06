3a3d57be32b224d8c74885790e6752daed71b955
goncalohgs <goncalohgs@gmail.com>
2024-10-06 21:27:07 +1100

    adds adds data to the qrcode, and add .make() to generete code

--- qr_code.py
+++ qr_code.py
@@ -4,3 +4,7 @@ linkedin_link = 'https://www.linkedin.com/in/goncalohsilva/'
 
 qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 
+qr.add_data(linkedin_link)
+qr.make()
+
+img = qr.make_image(fill_color = 'black', back_color = 'white')
